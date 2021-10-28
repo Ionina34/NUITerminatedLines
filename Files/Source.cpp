1a8dfd16d237a2d57c60ac3ae9b1bf3be9e9f324
Ionina34 <igoninadasha0315@mail.ru>
2021-10-28 15:23:31 +0500

    files

---
+++ Files/Source.cpp
@@ -0,0 +1,69 @@
+#include<iostream>
+#include<fstream>//File stream (Файловые потоки)
+using namespace std;
+
+//#define WRITE_TO_FILE
+//#define READ_FROM_FILE
+
+void main()
+{
+	setlocale(LC_ALL, "Rus");
+#ifdef WRITE_TO_WILE
+	//cout << "HelloWorld!" << endl;
+	ofstream fout("file.txt", ios::app);//Создаем и открываем поток
+	fout << "HelloWorld!";
+	fout.close();//Потоки обязательно нужно закртывать
+	system("more file.txt");
+	//"notepad file.txt"  
+#endif // WRITE_TO_WILE
+
+#ifdef READ_FROM_FILE
+	ifstream fin;//Создаем пток
+	fin.open("file.txt");
+	const unsigned int SIZE = 2048;
+	char sz_buffer[SIZE]{};//В эту строку будем читать из файла
+	//sz_ - Sring Zero (Строка, заканчивающаяся нулём)
+	if (fin.is_open())//Проверяем, открыт ли файл
+	{
+		//TODO:если открыт, будем читать
+		while (!fin.eof())//Пока НЕ конец файла -
+		{
+			// - будет производиться чтение
+			//eof()- End Of file
+			//fin >> sz_buffer;
+			fin.ignore();
+			fin.getline(sz_buffer, SIZE);
+			cout << sz_buffer << endl;
+		}
+	}
+	else
+	{
+		cerr << "Error: file not found" << endl;
+	}
+	fin.close();
+#endif // READ_FROM_FILE
+
+	ofstream fout("201 ready.txt");
+	ifstream fin("201 RAW.txt");
+	const unsigned int IP_SIZE = 16;
+	const unsigned int MAC_SIZE = 18;
+	char sz_ip_buffer[IP_SIZE] = {};
+	char sz_mac_buffer[MAC_SIZE] = {};
+	if (fin.is_open())
+	{
+		while (!fin.eof())
+		{
+			fin >> sz_ip_buffer;
+			fin >> sz_mac_buffer;
+			cout << sz_mac_buffer << "\t";
+			cout << sz_ip_buffer << "\n";
+		}
+	}
+	else
+	{
+		cerr << "Error: file not found" << endl;
+	}
+	fin.close();
+	fout.close();
+	system("notepad 201 ready.txt");
+} 
--- NUllTerminatedLines/Source.cpp
+++ NUllTerminatedLines/Source.cpp
@@ -7,6 +7,10 @@ using namespace std;
 #define Escape 27
 
 int StringLenght(char str[]);//Принимаепт строку и  возвращает размер строки(кол-во символов в строке);
+void to_upper(char str[]);
+void to_lower(char str[]);
+void shrink(char str[]);
+bool is_palindrome(char str[]);
 
 //void main()
 //{
@@ -37,6 +41,12 @@ void main()
 	//SetConsoleCP(866);
 	cout << str << endl;
 	cout << StringLenght(str) << endl;
+	/*to_upper(str);
+	cout << str << endl;
+	to_lower(str);
+	cout << str << endl;*/
+	shrink(str);
+	cout << str << endl;
 	/*cin >> key;
 	cout << (char)(key - 32);*/
 }
@@ -48,4 +58,38 @@ int StringLenght(char str[])
 	int i = 0;
 	for (; str[i]; i++);
 	return i;
+}
+void to_upper(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if ((str[i] >= 'a') && (str[i] <= 'z'))str[i] = str[i] - 32;
+		if ((str[i] >= 'а') && (str[i] <= 'я'))str[i] = str[i] - 32;
+	}
+}
+void to_lower(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if ((str[i] >= 'A') && (str[i] <= 'Z'))str[i] = str[i] + 32;
+		if ((str[i] >= 'А') && (str[i] <= 'Я'))str[i] = str[i] + 32;
+	}
+}
+void shrink(char str[])
+{
+	for (int i = 0; str[i]; i++)
+	{
+		if ((str[i] == ' ') && (str[i + 1] == ' '))
+		{
+			for (int j = i; str[j]; j++)
+			{
+				str[j] = str[j + 1];
+			}
+			i--;
+		}
+	}
+}
+bool is_palindrome(char str[])
+{
+	
 }
